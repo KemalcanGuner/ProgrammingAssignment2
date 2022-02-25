5ef60fcd166881e0a88bae69dc59f2f673e93c65
KemalcanGuner <48220433+KemalcanGuner@users.noreply.github.com>
2022-02-25 13:32:51 +0300

    Create cachematrix.R

---
+++ cachematrix.R
@@ -0,0 +1,35 @@
+#modified the assignment page example: 'caching mean of a vector'
+#and changed the input and variables
+
+makeCacheMatrix <- function(x = matrix()) {
+#this function is for creating the matrix, setting and getting the value
+#of matrix and setting and getting the inverse of the matrix
+
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setInv <- function(inverse) m <<- inverse
+        getInv <- function() m
+        list(set = set,
+             get = get,
+             setInv = setInv,
+             getInv = getInv)
+}
+
+cacheSolve <- function(x, ...) {
+#the function does try to fetch the inverse of the matrix 
+#if it really exists it gets it from the cache with a message and returns
+#if not calculates and sets the inverse itself
+        m <- x$getInv()
+        if (!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setInv(m)
+        m
+}
